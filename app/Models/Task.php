06b5e223092d7329c3fc047289c5712fc4d2bc42
Bender-0 <d_damra@hotmail.com>
2025-02-21 13:47:22 +0100

    Setup Models & Relationships

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\BelongsToManyRelationship;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    public function project(): BelongsTo 
+    { 
+        return $this->belongsTo(Project::class); 
+    }
+
+    public function category(): BelongsTo 
+    { 
+        return $this->belongsTo(Category::class); 
+    }
 }
