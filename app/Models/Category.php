c5ff3e483b6d8ce3e83547f1edd667fcbaf79663
Bender-0 <d_damra@hotmail.com>
2025-03-12 13:39:14 +0100

    fix method name typo

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,7 +10,7 @@ class Category extends Model
 {
     use HasFactory;
 
-    public function projects(): HasMany 
+    public function tasks(): HasMany 
     {
         return $this->hasMany(Task::class);
     }
